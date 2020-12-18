e3da7e375818a878ac0387845ca0a922f27cbe11
Vadim Savin <savinvadim1312@gmail.com>
2020-12-18 17:36:48 +0200

    map and list sync

--- src/screens/SearchResultsMap/index.js
+++ src/screens/SearchResultsMap/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect, useRef} from "react";
 import { View, FlatList, useWindowDimensions } from "react-native";
 import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
 import CustomMarker from "../../components/CustomMarker";
@@ -10,11 +10,40 @@ const SearchResultsMaps = (props) => {
 
   const [selectedPlaceId, setSelectedPlaceId] = useState(null);
 
+  const flatlist = useRef();
+  const map = useRef();
+
+  const viewConfig = useRef({itemVisiblePercentThreshold: 70})
+  const onViewChanged = useRef(({viewableItems}) => {
+    if (viewableItems.length > 0) {
+      const selectedPlace = viewableItems[0].item;
+      setSelectedPlaceId(selectedPlace.id)
+    }
+  })
+
   const width = useWindowDimensions().width;
+
+  useEffect(() => {
+    if (!selectedPlaceId || !flatlist) {
+      return;
+    }
+    const index = places.findIndex(place => place.id === selectedPlaceId)
+    flatlist.current.scrollToIndex({index})
+
+    const selectedPlace = places[index];
+    const region = {
+      latitude: selectedPlace.coordinate.latitude,
+      longitude: selectedPlace.coordinate.longitude,
+      latitudeDelta: 0.8,
+      longitudeDelta: 0.8,
+    }
+    map.current.animateToRegion(region);
+  }, [selectedPlaceId])
 
   return (
     <View style={{width: '100%', height: '100%'}}>
       <MapView
+        ref={map}
         style={{width: '100%', height: '100%'}}
         provider={PROVIDER_GOOGLE}
         initialRegion={{
@@ -36,6 +65,7 @@ const SearchResultsMaps = (props) => {
 
       <View style={{position: 'absolute', bottom: 10}}>
         <FlatList
+          ref={flatlist}
           data={places}
           renderItem={({item}) => <PostCarouselItem post={item} />}
           horizontal
@@ -43,6 +73,8 @@ const SearchResultsMaps = (props) => {
           snapToInterval={width - 60}
           snapToAlignment={"center"}
           decelerationRate={"fast"}
+          viewabilityConfig={viewConfig.current}
+          onViewableItemsChanged={onViewChanged.current}
         />
       </View>
     </View>
